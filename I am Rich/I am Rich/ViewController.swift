6b4f12590d1df66920f4e1a216e59d7d36340ab1
davidcode10 <davidedson2030@outlook.com>
2022-05-10 16:38:11 +0200

    app created

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by David Santiago on 10/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
